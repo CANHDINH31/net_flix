3aa79e67169e2a728096af24d1e263a1132a7d27
CANHDINH31 <88915565+CANHDINH31@users.noreply.github.com>
2022-06-11 22:05:44 +0700

    atn-11/6

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const moviesRoute = require("./routes/movies");
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ mongoose
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/movies", moviesRoute);
 
 app.listen(8800, () => {
   console.log("hello you");
